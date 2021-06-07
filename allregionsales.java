b79ef8d77d90bc043eaba60905f37c6f6981c2c8
swati-mca2020 <swati.narang@splashlearn.com>
2021-06-07 16:18:55 +0530

    first file added

---
+++ allregionsales.java
@@ -0,0 +1,8 @@
+package com.sales;
+
+public class allregionsales {
+    public static void  main(String[] args)
+    {
+        System.out.println("This file contains sales of all region");
+    }
+}
